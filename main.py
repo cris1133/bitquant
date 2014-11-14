a5b7ebd2c43b726f1b0b26d5b4127f8178975da3
Cristopher <cris11133@gmail.com>
2014-11-14 18:05:55 -0500

    3

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import decimal
+from chain_bitcoin import Chain
+chain = Chain(api_key_id='a62f58cb4594a5f284c7adb750e0f133')
 
 
 def getOrderBook():
@@ -8,4 +11,29 @@ def getTransactions():
 	return json.loads(requests.get("https://www.bitstamp.net/api/transactions/").content)
 def getTicker():
 	return json.loads(requests.get("https://www.bitstamp.net/api/ticker/").content)
+def getAddress(address):
+	return chain.get_address(address)
 
+class papertrade(object):
+	"""All transaction amounts are in BTC"""
+	def __init__(self):
+		self.buys = []
+		self.sells = []
+		## BTC in Satoshi
+		self.btc = 100000000
+		self.usd = 1000
+	def buy(self, amount):
+		price = float(getTicker()['ask'])
+		self.buys.append({"amount":amount, "price":price})
+		self.usd = self.usd - (amount * price)
+		self.btc = self.btc + amount
+	def sell(self, amount):
+		price = float(getTicker()['bid'])
+		self.sells.append({"amount":amount, "price":price})
+		self.btc = self.btc - amount
+		self.usd = self.usd + (amount * price)
+		
+## Testing stuff here
+def test():
+	orders = getOrderBook()
+	transactions = getTransactions()
